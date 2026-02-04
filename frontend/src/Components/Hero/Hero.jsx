cee1dae59bb0d209578a898c5c97d29d8dadde14
Cup0f <peterpolyak2000@gmail.com>
2026-02-04 16:53:12 +0100

    frontend config and skeleton

---
+++ frontend/src/Components/Hero/Hero.jsx
@@ -0,0 +1,30 @@
+﻿import Logo from "../../assets/man.png";
+
+const Hero = () => {
+    return (
+        <>
+            <div className="py-12 sm:py-0 dark:bg-black dark:text-white duration-300 overflow-hidden">
+                <div className="container min-h-100 flex relative">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 place-items-center relative z-10">
+                        <div className="space-y-5 lg:pr-20">
+                            <h1 className="text-4xl font-semibold">
+                                KÉSZÜLŐBEN PETI{" "}
+                                <span className="bg-clip-text text-transparent bg-linear-to-r from-sky-700 to-pink-700">
+                  PROFESSZINÁLIS EDZŐS OLDALA
+                </span>
+                            </h1>
+                            <p>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi porro temporibus odio exercitationem ea quibusdam voluptatibus obcaecati sint laboriosam labore. Iure nesciunt officia minima, eum nostrum amet cum recusandae inventore!
+                            </p>
+                        </div>
+                        <div>
+                            <img src={Logo} alt="" className="h-150"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Hero;
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,84 @@
+﻿import React from "react";
+import Logo from "../../assets/logo.png";
+import { HiMenuAlt1, HiMenuAlt3 } from "react-icons/hi";
+import DarkMode from "./DarkMode";
+
+const NavLinks = [
+    {
+        id: 1,
+        name: "Home",
+        link: "#",
+    },
+    {
+        id: 2,
+        name: "Products",
+        link: "#",
+    },
+    {
+        id: 3,
+        name: "Pricing",
+        link: "#",
+    },
+];
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = React.useState(false);
+    const toggleMenu = () => setShowMenu(!showMenu);
+
+    return (
+        <div className="relative z-9999 text-black dark:text-white duration-300">
+            <div className="container py-2 md:py-0">
+                <div className="flex justify-between items-center">
+                    <div className="flex items-center gap-3">
+                        <img src={Logo} alt="Logo" className="h-16" />
+                        <p className="text-3xl">
+                            GYM <span className="font-bold">World</span>
+                        </p>
+                    </div>
+
+                    <nav className="hidden md:block">
+                        <ul className="flex items-center gap-8">
+                            {NavLinks.map(({ id, name, link }) => {
+                                return (
+                                    <li key={id} className="py-4">
+                                        <a
+                                            href={link}
+                                            className="text-xl font-semibold hover:text-pink-700 py-2
+                    hover:border-b-2
+                    hover:border-sky-700
+                    transition-colors duration-500"
+                                        >
+                                            {name}
+                                        </a>
+                                    </li>
+                                );
+                            })}
+                            <DarkMode />
+                        </ul>
+                    </nav>
+
+                    <div className="md:hidden block">
+                        <div className="flex items-center gap-4">
+                            <DarkMode/>
+                            {showMenu ? (
+                                <HiMenuAlt1
+                                    onClick={toggleMenu}
+                                    className="cursor-pointer"
+                                    size={30}
+                                />
+                            ) : (
+                                <HiMenuAlt3
+                                    onClick={toggleMenu}
+                                    className="cursor-pointer"
+                                    size={30}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
